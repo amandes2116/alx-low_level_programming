7b0cac5c9b0b2e547b94bab68418736ae13435e7
amandes2116 <amandes246@gmail.com>
2023-02-16 07:46:56 -0800

    Betty 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - A programe that prints a line with puts function
+*Return:0 (success)
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzel,");
+return (0);
+
+}
