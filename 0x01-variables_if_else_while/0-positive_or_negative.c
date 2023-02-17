253611763c1c0ce603d3afe950d795508a96a8c3
amandes2116 <amandes246@gmail.com>
2023-02-17 10:01:34 -0800

    Postive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
-*main - A program that prints random number.
-*the number stored in the variable n is positive or negative.
-*Return:0 (success)
+*main - programe that print random number.
+*Return:0(success)
 */
 int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAX /2;
-if((n % 10)> 5)
+n = rand() - RAND_MAX / 2;
+if (n > 0)
 {
-printf("The last digit of %d is %d and is graterthan 5\n", n, n%10);
+printf("%d is positive\n", n);
 }
-else if((n % 10) < 6 && (n % 10) !=0)
+else if (n == 0)
 {
-printf("The last digit of %d is %d and less than 6 and not 0\n", n, n%10);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("The last digit of %d is %d and is 0\n", n, n%10);
+printf("%d is negative\n", n);
 }
-return 0;
+return (0);
 }
