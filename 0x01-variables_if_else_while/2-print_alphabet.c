298667ab12fd6492da1ea5d2613f2ee26be45c11
amandes2116 <amandes246@gmail.com>
2023-02-17 10:58:05 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - A program that prints the alpabet in lowercase.
+*Return:0(success)
+*/
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
