8bdcc8a052ce435b440f19816b8ed0eaad4c2e0a
amandes2116 <amandes246@gmail.com>
2023-02-17 22:17:33 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - A programe that print single digit numbers of base 10 starting from 10.
+*Return:0(success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf('\n');
+return (0);
+}
