eeeaa7ded31a60f6314ab841bf68c4f968e234aa
amandes2116 <amandes246@gmail.com>
2023-02-21 08:55:54 -0800

    print last digit

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-int r;
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+	int r;
+
+	r = print_last_digit(98);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
